3f361f72167290683bf07d414adca1d7e049cf59
Raphael-Catacutan <100451255+Raphael-Catacutan@users.noreply.github.com>
2022-05-25 18:29:47 +0800

    Add files via upload

---
+++ Q4W5- Assimilation.js
@@ -0,0 +1,21 @@
+var anim = 2, set1 = 0, set2 = 0 
+document.getElementById('box1').setAttribute('onclick', ' nextSlide(anim % 2)')
+function nextSlide(state) {
+    anim += 1;
+    if (state) {
+        set1 = "back"
+        set2 = "front"
+        document.getElementById('box1').setAttribute('onclick', ' nextSlide(anim % 2)')
+        document.getElementById('box2').removeAttribute('onclick')
+    }
+    else {
+        set1 = "front"
+        set2 = "back"
+        document.getElementById('box2').setAttribute('onclick', ' nextSlide(anim % 2)')
+        document.getElementById('box1').removeAttribute('onclick')
+    }
+    document.getElementById('box1').classList.remove(set2)
+    document.getElementById('box2').classList.remove(set1)
+    document.getElementById('box1').classList.add(set1)
+    document.getElementById('box2').classList.add(set2)
+}
